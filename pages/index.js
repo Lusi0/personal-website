d4bf54848812503ff8407abf664809b20c0ff998
Lusi0 <ItsLusio@gmail.com>
2022-04-03 15:12:07 -0700

    got rid of junk

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Navbar from '../componets/navbar'
 import { useRouter } from 'next/router'
 import { useState } from 'react'
 
