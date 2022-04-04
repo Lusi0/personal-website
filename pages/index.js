b23f008c6adcc2a13e15b5d309a28cb3a15f3ebf
Lusi0 <ItsLusio@gmail.com>
2022-04-03 17:51:10 -0700

    more stuff good look

--- pages/index.js
+++ pages/index.js
@@ -12,12 +12,16 @@ export default function Home({ myrepos, myaccount}) {
   // states to show and hide the repoholder
   const [showmain, setshowmain] = useState(true);
   const [showrepos, setShowrepos] = useState(false);
+  const [showaccounts, setShowaccounts] = useState(false);
+  const [showblog, setblog] = useState(false);
 
   
   function toggleContent(e) {
     // disable all content and show the one that was clicked
     setShowrepos(false);
     setshowmain(false);
+    setShowaccounts(false);
+    setblog(false);
     if (e === 'repos') {
       setShowrepos(true);
     }
@@ -26,12 +30,22 @@ export default function Home({ myrepos, myaccount}) {
       setshowmain(true);
     }
 
+    if (e === 'accounts') {
+      setShowaccounts(true);
+    }
+
+    if (e === 'blog') {
+      setblog(true);
+    }
+
+
   }
 
   return (
     <div className={styles.container}>
       <Head>
         <title>Avery&#39;s Portfolio</title>
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       
       <main>
@@ -40,12 +54,12 @@ export default function Home({ myrepos, myaccount}) {
             <div className="navbar-container">
 
                 <div className="navbar-links">
-                    <button className="link" onClick={() => toggleContent('main')}>Avery!</button>
+                    <button className="link" onClick={() => toggleContent('main')}>Avery</button>
                     
-                    <button className="link" onClick={() => toggleContent('repos')}>{myaccount.public_repos} Reposotories!</button>
+                    <button className="link" onClick={() => toggleContent('repos')}>{myaccount.public_repos} Reposotories</button>
 
-                    <button className="link"> Accounts!</button>
-                    <button className="link"> Cool Plog Posts!</button>
+                    <button className="link" onClick={() => toggleContent('accounts')} > Accounts</button>
+                    <button className="link" onClick={() => toggleContent('blog')}> Cool Plog Posts</button>
                     
 
                 </div>
@@ -55,8 +69,9 @@ export default function Home({ myrepos, myaccount}) {
         {/* main content */}
         { showmain ? (
           <div className="main">
-            <h1>Hi there! I am Avery Smith :)</h1>
-            <p>I am a computer science student who will be attending stevens institute of technology next fall. (SAAS class of &#39;22)</p>
+            <h1 className='main-title'>Hi there! I am Avery Smith :)</h1>
+            <p>I am a computer science student who will be attending stevens institute of technology next fall. (SAAS class of &#39;22) </p>
+
           </div>
         ) : null }
 
@@ -66,11 +81,27 @@ export default function Home({ myrepos, myaccount}) {
             <div className="repoholder">
                 { myrepos.map(repo => (
                     <div key={repo.name} className="repo">
-                        <a href={repo.html_url} >{repo.name}</a>
+                      {/* open in new page */}
+                        <a href={repo.html_url} target="_blank" >{repo.name}</a>
                     </div>
                 ))}
             </div>
         : null}
+
+        {/* accounts */}
+        {/* if showaccounts is true then display accounts */}
+        {showaccounts ?
+            <div className="accounts">
+            </div>
+        : null}
+
+        {/* blog */}
+        {/* if showblog is true then display blog */}
+        {showblog ?
+            <div className="blog">
+            </div>
+        : null}
+
 
 
       </main>
